b8662706a010e6ceefcbf5481ed392d37410b650
Karla R <karla.y.reyes12@gmail.com>
2018-07-23 16:16:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,14 +3,13 @@ class Anagram
   
   attr_accessor :word
   
-  @@all = []
-  
+
   def initialize(word)
     @word = word
-    @@all << self
+    @match = []
   end
   
-  def self.match()
+  
   
   
   
